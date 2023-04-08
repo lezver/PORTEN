50e4250e5d2596021d71cf56773392b325b17182
Vladislav <102545184+lezver@users.noreply.github.com>
2023-04-08 12:43:18 +0300

    fixed of menu burger

--- js/menu-btn.js
+++ js/menu-btn.js
@@ -1,17 +1,28 @@
 const menuBtnRef = document.querySelector(".nav__btn");
 const menuRef = document.querySelector(".nav__items");
+// const burgerBtnRef = document.querySelector(".burger");
+// const crossBtnRef = document.querySelector(".cross");
 
-const toggleMenu = (e) => menuRef.classList.toggle("toggle-menu");
+const [burger, cross] = menuBtnRef.children;
+
+// console.log(burger);
+
+const toggleMenu = () => {
+	menuRef.classList.toggle("toggle-menu");
+	burger.classList.toggle("burger");
+	cross.classList.toggle("cross");
+};
 
 menuBtnRef.addEventListener("click", toggleMenu);
 
 const toggleOfClass = (e) => {
-	console.log(e.target);
 	if (
 		e.target.className === "nav__link" ||
 		e.target.className === "nav__link nav__link--active"
 	)
 		menuRef.classList.remove("toggle-menu");
+	burger.classList.remove("burger");
+	cross.classList.add("cross");
 };
 
 menuRef.addEventListener("click", toggleOfClass);
